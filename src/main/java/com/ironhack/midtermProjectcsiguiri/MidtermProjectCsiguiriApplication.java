79f44e6a9f3ecc2b40e2b5d2dbcfcda2aaa98ee0
Carolina Siguiri <siguiricarolina@gmail.com>
2021-02-05 21:20:09 +0000

    base limpia creada

---
+++ src/main/java/com/ironhack/midtermProjectcsiguiri/MidtermProjectCsiguiriApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.midtermProjectcsiguiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermProjectCsiguiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtermProjectCsiguiriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/midtermProjectcsiguiri/MidtermProjectCsiguiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.midtermProjectcsiguiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermProjectCsiguiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
